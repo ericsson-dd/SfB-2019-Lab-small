91a6cfe42a56ca71abe01cad2e40966a5764f9fb
ericsson-dd <ericschoene@live.de>
2021-09-07 14:45:32 +0200

    init base files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SfB-2019-Lab-small
+
+
+
+Azure Filestorage access Token Config für backend storage
+$ENV:ARM_ACCESS_KEY = ""
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+    description = "Azure Subscription ID"
+    type = string
+}
+
+variable "tenant_id" {
+    description = "Tenant ID"
+    type = string
+}
+
+variable "client_id" {
+    description = "Application Registration Client ID"
+    type = string
+}
+
+variable "client_secret" {
+    description = "Application Registration Client Secret"
+    type = string
+}
